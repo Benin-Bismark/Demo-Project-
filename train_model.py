5d881471ccfe286877ecd8905b1b14553fb050b4
Benin-Bismark <beninbismark@gmail.com>
2026-09-26 17:28:35 +0300

    train the Dataset with the right model Logistic Regression

--- train_model.py
+++ train_model.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import classification_report, accuracy_score
 import joblib
 
@@ -11,7 +11,7 @@ y = df["result"]
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-model = RandomForestClassifier(n_estimators=100, random_state=42)
+model = LogisticRegression(max_iter=1000)
 model.fit(X_train, y_train)
 
 y_pred = model.predict(X_test)
